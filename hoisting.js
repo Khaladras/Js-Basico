b11017f85f9c34652509bc3f9d1cbafdb7a30db6
Marcos Bruno <marcosferreyra82@gmail.com>
2022-10-12 18:25:55 -0300

    first commit

---
+++ hoisting.js
@@ -0,0 +1,28 @@
+
+    var miNombre;  //Declarar
+    miNombre = "Marcos Bruno";  //inicializar
+
+    //CONSOLE.LOG 
+    //funcion del navegador..imprime en la consola resultados,variables,etc
+    -------------------------------------      
+    
+    EJEMPLOS
+    function nombreDelGato(nombre) {
+        console.log("El nombre de mi gato es " + nombre);
+      }
+      
+      nombreDelGato("CoCo");
+      /*
+      El resultado del código es: "El nombre de mi gato es CoCo"
+      */
+
+----------------------------------------------
+
+      nombreDelGato("Dumas");
+
+      function nombreDelGato(nombre) {
+        console.log("El nombre de mi gato es " + nombre);
+      }
+      /*
+      El resultado del código es: "El nombre de mi gato es Dumas"
+      */
