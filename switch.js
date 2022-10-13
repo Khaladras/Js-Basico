aa4f54ee832a9952c67ee4926ba1d58c97c6f1b7
Marcos Bruno <marcosferreyra82@gmail.com>
2022-10-12 23:17:47 -0300

    addswitch

---
+++ switch.js
@@ -0,0 +1,32 @@
+   var numero=1
+    
+    switch (numero) {
+        case 1:
+            console.log("Soy un UNO")
+            break;
+        case 5:
+        console.log("soy un Fives");
+            break;
+        case 100:
+        console.log("soy un cien!")
+        default:
+        console.log("Nada q ver")
+    }
+    //consola: Soy un UNO
+    var numero=5
+    
+    switch (numero) {
+        case 1:
+            console.log("Soy un UNO")
+            break;
+    case 5:
+        console.log("soy un Fives");
+            break;
+        case 100:
+        console.log("soy un cien!")
+        default:
+        console.log("Nada q ver")
+    }
+   //consola: Soy un Fives
+   
+                //Sin los breaks valida todos los casos
