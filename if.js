6979e9c02c043dc838071d6c29c112566065dfd9
Marcos Bruno <marcosferreyra82@gmail.com>
2022-10-12 21:31:59 -0300

    commit2

---
+++ if.js
@@ -0,0 +1,55 @@
+
+    var edad = 18;
+
+    if (edad ===18){console.log("Te felicito votaste x 1ra vez!")}
+    
+    else if (edad >18){
+        console.log ("gracias x darnos tu voto");
+    }
+    else {
+        console.log ("voto no permitido x baja edad")
+    }
+
+// Te felicito votaste x 1ra vez!
+
+    var edad = 28;
+
+    if (edad ===18){console.log("Te felicito votaste x 1ra vez!")}
+    else if (edad >18){
+        console.log ("gracias x darnos tu voto");
+    }
+    else {
+        console.log ("voto no permitido x baja edad")
+    }
+
+// gracias x darnos tu voto
+
+    var edad = 17;
+
+    if (edad ===18){console.log("Te felicito votaste x 1ra vez!")}
+    else if (edad >18){
+        console.log ("gracias x darnos tu voto");
+    }
+    else {
+        console.log ("voto no permitido x baja edad")
+    }
+// voto no permitido x baja edad
+     
+        //OPERADOR TERNARIO//
+    condition ? true:false;
+    
+var numero=1;
+var resultado = numero ===1 ? "Si soy un UNO" : "No,no soy un uno";
+
+var numero=1;
+var resultado = numero ===1 ? "Si soy un UNO" : "No,no soy un uno";
+console.log(resultado);
+//Si soy un UNO
+            
+var numero=3;
+var resultado = numero ===1 ? "Si soy un UNO" : "No,no soy un uno";
+console.log(resultado)
+//No,no soy un uno
+
+
+
---
+++ kchipum.js
@@ -0,0 +1,7 @@
+var user
+var pc 
+var tijera=1
+var papel =5
+var piedra=10
+
+function Empate(tijera===tijera,papel===papel,piedra===piedra) {console.log("hay empate!");}
